5c1eb6d42594f867caa3736298abff1afae8e608
Lucian-77 <lucianshambhu@gmail.com>
2025-03-04 14:43:39 -0500

    Wrote the code

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -2,8 +2,15 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "How many cookies do you wish to make?\n\nNumber of cookies: ";
+    double numOCookies;
+    std::cin >> numOCookies;
+    double ratio = numOCookies / 48;
+    double cOSugar = 1.5 * ratio, cOButter = 1 * ratio, cOFlour = 2.75 * ratio;
+    std::cout << std::setprecision(3) << "\nYou will need:\n" << cOSugar << " cups of sugar,\n" << cOButter << " cups of butter,\nAnd " << cOFlour << " cups of flour.\n";
+    return 0;
 }
