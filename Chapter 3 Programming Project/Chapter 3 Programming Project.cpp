631498c8c4e0c1d99908df2e8c172456f1b49415
Lucian-77 <lucianshambhu@gmail.com>
2025-03-05 19:50:09 -0500

    MAde everything neat

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -6,17 +6,18 @@
 
 int main()
 {
-    std::cout << std::left << std::setw(38) << "What is your principle amount?" << std::right << "$ ";
+    std::cout << std::left << std::setw(40) << "What is your principle amount?" << std::right << "$ ";
     double prince;
     std::cin >> prince;
-    std::cout << std::left << std::setw(39) << "\nWhat is your interest rate?" << std::right << "% ";
+    std::cout << std::left << std::setw(41) << "\nWhat is your interest rate?" << std::right << "% ";
     double intRate;
     std::cin >> intRate;
-    std::cout << "\nHow many times will it compound? " << "amount: ";
+    std::cout << std::left << std::setw(35) << "\nHow many times will it compound? " << std::right << "Amount: ";
     double nOComp;
     std::cin >> nOComp;
     double total = prince * pow((1 + ((intRate / 100) / nOComp)), nOComp);
     double interest = total - prince;
-    std::cout << std::fixed << std::setprecision(2) << "\nEarned interest: $" << interest << "\n\nTotal amount in savings: $" << total << "\n";
+    std::cout << "\nReport:" << std::left << std::setw(41) << std::fixed << std::setprecision(2) << "\nEarned interest:" << std::right << "$ " << std::setw(7)
+        << std::right << interest << std::left << std::setw(41) << "\nTotal amount in savings:" << std::right << "$ " << total << "\n";
     return 0;
 }
